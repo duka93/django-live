b98e5be7475522ec69f9d37af707b1a28527d981
Duka93 <dusantime@gmail.com>
2022-10-20 12:54:40 +0200

    small update

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls"),name="login"),
-    path("/signup",views.Signup.as_view(),name="signup")
+    path("signup/",views.Signup.as_view(),name="signup")
 ]
